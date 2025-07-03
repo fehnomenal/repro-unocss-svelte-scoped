f2ca35aa038e384db7e206470203f6f8b36475ed
Andreas Fehn <fehnomenal@fehn.systems>
2025-07-03 15:49:27 +0200

    setup unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import unocss from '@unocss/svelte-scoped/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [unocss(), sveltekit()],
 });
